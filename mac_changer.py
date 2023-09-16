46d7db49cff8275703896f641d82256fe3bb13d7
Saher Muhamed <81532683+SaherMuhamed@users.noreply.github.com>
2023-09-16 09:38:53 +0300

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,54 +1,61 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import re
+import sys
 import subprocess
 from optparse import OptionParser
 
+if sys.version_info < (3, 0):
+    sys.stderr.write("\nYou need python 3.0 or later to run this script\n")
+    sys.stderr.write(
+        "Please update and make sure you use the command python3 mac_changer.py -i <interface> -m <mac>\n\n")
+    sys.exit(0)
 
-def get_argument():
+
+def args():
     parser = OptionParser()
-    parser.add_option("-i", "--interface", dest="interface",
-                      help="Specify an interface to change its MAC. Example: --interface wlan0")
-    parser.add_option("-m", "--mac", dest="mac_address",
-                      help="enter a new MAC Address to your NAT interface. Example: --mac 70:00:00:00:00:08")
+    parser.add_option("-i", "--interface", dest="interface", help="Specify an interface to change its MAC. Example: "
+                                                                  "--interface wlan0")
+    parser.add_option("-m", "--mac", dest="mac_address", help="Enter a new MAC Address to your NAT interface. Example: "
+                                                              "--mac 70:00:00:00:00:08")
     (options, arguments) = parser.parse_args()
     if not options.interface:
-        parser.error("[-] Please specify your NAT card, or type it correctly, ex: --interface wlan0")
+        parser.error(msg="[-] Please specify your NAT card, or type it correctly, ex: --interface wlan0")
     elif not options.mac_address:
-        parser.error("[-] Please enter your new MAC Address, or type it correctly, ex: --mac 70:00:00:00:00:08")
+        parser.error(msg="[-] Please enter your new MAC Address, or type it correctly, ex: --mac 70:00:00:00:00:08")
     return options
 
 
-def change_mac(interface, new_mac):
-    # subprocess.call(f"ifconfig {interface} down", shell=True)
-    # subprocess.call(f"ifconfig {interface} hw ether {new_mac}", shell=True)
-    # subprocess.call(f"ifconfig {interface} up", shell=True)
+def change_mac(iface, mac):
+    print("[+] Changing MAC address for " + iface + " to " + mac)
+    # subprocess.call("ifconfig " + iface + " down", shell=True)
+    # subprocess.call("ifconfig " + iface + " hw ether " + mac, shell=True)
+    # subprocess.call("ifconfig " + iface + " up", shell=True)
 
-    # TODO: These 3 commands are a much more secure version of the 3 command above.
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
-    print(f"[+] Changing MAC Address for {interface} to {new_mac}\n")
+    # these 3 commands are much more secure version
+    subprocess.call(["ifconfig", iface, "down"])  # execute the command in foreground
+    subprocess.call(["ifconfig", iface, "hw", "ether", mac])
+    subprocess.call(["ifconfig", iface, "up"])
 
 
-def get_current_mac(interface):
-    ifconfig_command_result = subprocess.check_output(["ifconfig", interface])
-    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_command_result.decode("UTF-8"))
+def search_mac_regex(iface):
+    ifconfig_result = subprocess.check_output(["ifconfig", iface])  # execute the command and return the output in bytes
+    mac_address_search_result = re.search(pattern=r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", string=str(ifconfig_result))
+
     if mac_address_search_result:
         return mac_address_search_result.group(0)
     else:
         print("[-] Couldn't find MAC address, try another NAT card.\n")
 
 
-option = get_argument()
+try:
+    option = args()
+    current_mac_before = search_mac_regex(iface=option.interface)
+    print("\n[+] Current MAC: " + str(current_mac_before))
 
-current_mac = get_current_mac(option.interface)
-print(f"[+] Current MAC: {current_mac}")
-
-change_mac(interface=option.interface, new_mac=option.mac_address)
-
-current_mac = get_current_mac(option.interface)
-if current_mac == option.mac_address:
-    print(f"[+] MAC Address was successfully changed to {current_mac}\n")
-else:
-    print("[-] Couldn't change MAC Address\n")
+    change_mac(iface=option.interface, mac=option.mac_address)
+    current_mac_after = search_mac_regex(iface=option.interface)
+    if current_mac_after == option.mac_address:
+        print("[+] MAC address was successfully changed to " + current_mac_after + "\n")
+except subprocess.CalledProcessError:
+    sys.stderr.write("[-] Couldn't find " + option.interface + ", try another NAT card.\n")
